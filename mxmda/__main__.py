b8ae3b836278fa2b4cc99e1c53eab743c5961b93
Olof Johansson <olof@ethup.se>
2023-03-12 20:59:30 +0100

    Add pyproject.toml
    This also has the side effect of messing up my __main__; because
    setuptools doesn't support __main__ modules, the entrypoints must refer
    to a function.
    
    Reference: https://github.com/pypa/setuptools/issues/1995

--- mxmda/__main__.py
+++ mxmda/__main__.py
@@ -6,7 +6,17 @@ import mxmda
 from mxmda.app import command, parse_args
 from mxmda.errors import UserError
 
-if __name__ == '__main__':
+# https://github.com/pypa/setuptools/issues/1995
+# Proper solution specified in comment by @Xophmeister:
+# > In the current system, you have to delimit module and the
+# > entrypoint callable with a colon. What if the logic was
+# > something like:
+# >
+# > Split on:
+# >  If there are two components, then proceed as currently;
+# >  If there is only one component, then interpret this as
+# >  a direct module invocation?
+def issue_1995():
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s  [%(levelname)-8s] %(name)s: %(message)s',
@@ -18,3 +28,6 @@ if __name__ == '__main__':
     except UserError as exc:
         logging.getLogger(mxmda.__name__).error(exc)
         sys.exit(1)
+
+if __name__ == '__main__':
+    issue_1995()
